8d6f5e921cf0b6e03540c503818494948e860d16
PhilipKirner <philip.kirner@gmail.com>
2020-04-23 16:24:24 -0600

    clean up home page

--- frontend/scada/src/App.js
+++ frontend/scada/src/App.js
@@ -16,8 +16,6 @@ function App() {
     <div className="App">
 
       <header className="App-header">
-        <h1>GasBois, Check your Gas!</h1>
-        <img src={mockup} alt="it looks like this." />
         <Container style={style}>
           <Row>
             <Col>
@@ -28,42 +26,6 @@ function App() {
             </Col>
           </Row>
         </Container>
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is a completed SCADA Wireframe, we can all go back to bed now.
-        </a>
-        <a
-            className="App-link"
-            href="https://www.npmjs.com/package/react-thermometer-chart"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          This is the Temperature Widget
-        </a>
-        <a
-            className="App-link"
-            href="https://www.npmjs.com/package/react-gauge-chart"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          This is the Pressure Widget
-        </a>
-        <a
-            className="App-link"
-            href="https://www.npmjs.com/package/react-bulb"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          This is the Indicator Bulb Widget
-        </a>
       </header>
     </div>
   );
